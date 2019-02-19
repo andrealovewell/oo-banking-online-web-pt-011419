dcc8e1762e5de02ef3608cb6cebded36e0549c4c
Andrea Lovewell <andy.lovewell@gmail.com>
2019-02-19 18:11:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,6 +10,26 @@ class Transfer
   end
 
   def valid?
-    @sender.valid?&& @reciever.valid? ? true : false
+    @sender.valid? && @reciever.valid? ? true : false
   end
+
+  def execute_transaction
+   if @sender.balance > @amount && @status == "pending"
+     @sender.balance -= @amount
+     @receiver.balance += @amount
+     @status = "complete"
+   else
+     @status = "rejected"
+     return "Transaction rejected. Please check your account balance."
+   end
+ end
+
+ def reverse_transfer
+   if @status == "complete"
+     @sender.balance += @amount
+     @receiver.balance -= @amount
+     @status = "reversed"
+   end
+ end
+
 end
